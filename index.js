1d425460b0a69b1f4304ec8a6cf3d6ee04a585e4
Adrian Orive <aorive07@gmail.com>
2019-12-18 23:28:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function sayHello(){
+  console.log('Hello');
+}
+sayHello();
+function sayHelloToIsabel(){
+  console.log("Hello, Isabel!");
+}
+sayHelloToIsabel();
+function sayHelloToSofia(){
+  console.log("Hello, Sofia!");
+}
+sayHelloToSofia();
+function sayHelloToBrendan(){
+  console.log("Hello, Brendan!");
+}
+sayHelloToBrendan();
